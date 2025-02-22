486ea0b98ca85c990505e81c2eed78b13918b63d
the-dg04 <devgup04@gmail.com>
2025-02-23 01:17:32 +0530

    added icons

--- src/TechnexScanner.jsx
+++ src/TechnexScanner.jsx
@@ -1,46 +1,44 @@
 import { Scanner } from "@yudiel/react-qr-scanner";
+import { Badge, BadgeAlert, BadgeCheck, BadgeMinus } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const BACKEND_URL=import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export default function TechnexScanner() {
   const { event_name } = useParams();
-  const [scanResult, setScanResult] = useState({name:"",username:""});
+  const [scanResult, setScanResult] = useState({ name: "", username: "" });
   const [status, setStatus] = useState("idle");
 
   const handleScan = async (result) => {
     setStatus("scanning...");
 
-    const res=await fetch(`${BACKEND_URL}/api/user/verify-qr`,
-        {
-            method:"POST",
-            headers:{"Content-type":"application/json"},
-            body:JSON.stringify({
-                "qr_token":result[0].rawValue,
-                "event_name":event_name
-            })
-        }
-    )
-    if(res.status==200){
-        const resJSON=await res.json()
-        console.log(resJSON)
-        setStatus("verified");
-        setScanResult(resJSON)
-    }else if(res.status==401){
-        setStatus("user not registered for this event")
-        setScanResult({name:"",username:""})
-    }else{
-        setStatus("invalid")
-        setScanResult({name:"",username:""})
+    const res = await fetch(`${BACKEND_URL}/api/user/verify-qr`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        qr_token: result[0].rawValue,
+        event_name: event_name,
+      }),
+    });
+    if (res.status == 200) {
+      const resJSON = await res.json();
+      console.log(resJSON);
+      setStatus("verified");
+      setScanResult(resJSON);
+    } else if (res.status == 401) {
+      setStatus("user not registered for this event");
+      setScanResult({ name: "", username: "" });
+    } else {
+      setStatus("invalid");
+      setScanResult({ name: "", username: "" });
     }
-    
   };
 
   return (
     <div className="max-w-[740px] mx-auto w-full h-screen">
       <div className="flex items-center justify-center">
-        <div className="flex justify-center items-center w-[80%]">
+        <div className="flex justify-center items-center w-full md:w-[80%]">
           <Scanner
             formats={["qr_code"]}
             onScan={handleScan}
@@ -49,22 +47,24 @@ export default function TechnexScanner() {
         </div>
       </div>
       <div className="w-full flex justify-center">
-        <div className="flex flex-col justify-center items-center bg-gray-100 w-[80%]">
+        <div className="flex flex-col justify-center items-center bg-gray-100 w-full md:w-[80%] p-2">
+          <div className="w-full flex justify-center">
+            {status==="verified" && <span className="text-green-500"><BadgeCheck/></span> }
+            {(status==="invalid" || status==="user not registered for this event") && <span className="text-red-500"><BadgeAlert /></span> }
+            {status==="idle" && <span className="text-gray-500"><Badge /></span> }
+            {status==="scanning..." && <span className="text-blue-500"><BadgeMinus /></span> }
+          </div>
           <div className="w-full flex">
             <h2 className="text-xl font-bold">Status:</h2>
             <p className="ml-2 text-lg">{status}</p>
           </div>
           <div className="w-full flex">
             <h2 className="text-xl font-bold">Name:</h2>
-            <p className="ml-2 text-lg">
-              {scanResult.name}
-            </p>
+            <p className="ml-2 text-lg">{scanResult.name}</p>
           </div>
           <div className="w-full flex">
             <h2 className="text-xl font-bold">Username:</h2>
-            <p className="ml-2 text-lg">
-              {scanResult.username}
-            </p>
+            <p className="ml-2 text-lg">{scanResult.username}</p>
           </div>
         </div>
       </div>
